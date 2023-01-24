3f3d30103eeedd436c946d10d14e074cc84a9b7d
joaoneto-hub <joaorodrigues8182@gmail.com>
2023-01-23 22:28:57 -0300

    Correção link da API

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,4 @@
-export const STANDART_URL = 'https://mks-frontend-challenge-api.herokuapp.com/api/v1/products';
+export const STANDART_URL = 'https://mks-challenge-api-frontend.herokuapp.com/api/v1/products';
 
 export const getAll = `${STANDART_URL}?page=1&rows=8&sortBy=id&orderBy=ASC`;
 
